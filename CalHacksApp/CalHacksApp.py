35b73ffbcb0e7fa29c612fd3f7c527321f531050
murtaza1 <murtaza1@msu.edu>
2024-10-19 19:44:19 -0400

    symptoms chat

--- CalHacksApp/CalHacksApp.py
+++ CalHacksApp/CalHacksApp.py
@@ -1,15 +1,56 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+# """Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+# import reflex as rx
+
+# from rxconfig import config
+
+
+# class State(rx.State):
+#     """The app state."""
+
+#     ...
+
+
+# def index() -> rx.Component:
+#     # Welcome Page (Index)
+#     return rx.container(
+#         rx.color_mode.button(position="top-right"),
+#         rx.vstack(
+#             rx.heading("Get your diagnosis!", size="9"),
+#             rx.text(
+#                 "Please input your symptoms",
+#                 rx.code(f"{config.app_name}/{config.app_name}.py"),
+#                 size="5",
+#             ),
+#             rx.link(
+#                 rx.button("Check out our docs!"),
+#                 href="https://reflex.dev/docs/getting-started/introduction/",
+#                 is_external=True,
+#             ),
+#             spacing="5",
+#             justify="center",
+#             min_height="85vh",
+#         ),
+#         rx.logo(),
+#     )
+
+
+# app = rx.App()
+# app.add_page(index)
+
+
 
 import reflex as rx
-
 from rxconfig import config
-
 
 class State(rx.State):
     """The app state."""
+    
+    symptoms: str = ""
 
-    ...
-
+    def submit_symptoms(self):
+        # This could trigger a diagnosis processing (e.g., call an API or process the input)
+        print(f"Symptoms submitted: {self.symptoms}")
 
 def index() -> rx.Component:
     # Welcome Page (Index)
@@ -19,14 +60,12 @@ def index() -> rx.Component:
             rx.heading("Get your diagnosis!", size="9"),
             rx.text(
                 "Please input your symptoms",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
+            # Add a textbox for user input, binding it to the symptoms state
+            rx.input(placeholder="Enter your symptoms here...", on_change=State.set_symptoms),
+            # Add a button that will submit the symptoms
+            rx.button("Submit", on_click=State.submit_symptoms),
             spacing="5",
             justify="center",
             min_height="85vh",
@@ -34,6 +73,6 @@ def index() -> rx.Component:
         rx.logo(),
     )
 
-
 app = rx.App()
 app.add_page(index)
+
